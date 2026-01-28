cfc9145c065cfd8b63d2a3d5e7a6db1c6869e340
Anna Dagbaeva <d0n.mei@ya.ru>
2026-01-28 12:35:50 +0900

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+import {initialCards} from "./cards.js";
+const createCard = (data, deleteCallback) => {
+  const template = document.querySelector('#card-template');
+  const newCardElement = template.content.cloneNode(true);
 
-// @todo: DOM узлы
+  const imageElement = newCardElement.querySelector('.card__image');
+  const titleElement = newCardElement.querySelector('.card__title');
 
-// @todo: Функция создания карточки
+  imageElement.src = data.link;
+  imageElement.alt = data.name;
+  titleElement.textContent = data.name;
 
-// @todo: Функция удаления карточки
+  const deleteButton = newCardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', (el) => {
+    const item = el.target.closest('.places__item');
+    deleteCallback(item);
+  });
+  return newCardElement;
+};
 
-// @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+const removeCard = element => {
+  element.remove();
+};
+
+const renderInitialCards = () => {
+  initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, removeCard);
+    placesList.appendChild(cardElement);
+  });
+};
+
+renderInitialCards();
